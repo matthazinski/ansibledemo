29b4fec7fed12ad4abef994fc183666155c5f9f9
Matt Hazinski <matt@matthazinski.com>
2015-11-03 20:44:22 -0500

    Add -p to mkdir for quieter output

--- make_lxc_sshable.sh
+++ make_lxc_sshable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in `lxc-ls`
 do
-    mkdir /var/lib/lxc/$i/rootfs/root/.ssh/
+    mkdir -p /var/lib/lxc/$i/rootfs/root/.ssh/
     cp /root/.ssh/id_rsa.pub /var/lib/lxc/$i/rootfs/root/.ssh/authorized_keys
 done
 
