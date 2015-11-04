1a41c424cbae36094a15097eababae8ba9c656fc
Matt Hazinski <matt@matthazinski.com>
2015-11-03 21:16:15 -0500

    Add python install to make_lxc_sshable.sh

--- make_lxc_sshable.sh
+++ make_lxc_sshable.sh
@@ -3,5 +3,6 @@ for i in `lxc-ls`
 do
     mkdir -p /var/lib/lxc/$i/rootfs/root/.ssh/
     cp /root/.ssh/id_rsa.pub /var/lib/lxc/$i/rootfs/root/.ssh/authorized_keys
+    lxc-attach -n $i apt-get -y install python
 done
 
